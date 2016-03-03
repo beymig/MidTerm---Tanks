d63d87fc71110a4f22dbec501c1370b886d2548a
beymig <beymig@gmail.com>
2016-03-02 19:50:55 -0800

    added score and lives

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
 	// PUBLIC INSTANCE VARIABLES
 	public int tankCount;
 	public GameObject tank;
-	
-	// Use this for initialization
-	void Start () {
+    private int _scoreValue;
+    private int _livesValue;
+    public EnemyController enemy;
+    // PUBLIC ACCESS METHODS
+
+    public Text LivesLabel;
+    public Text ScoreLabel;
+    public int ScoreValue
+    {
+        get
+        {
+            return this._scoreValue;
+        }
+
+        set
+        {
+            this._scoreValue = value;
+            this.ScoreLabel.text = "Score: " + this._scoreValue;
+        }
+    }
+
+    public int LivesValue
+    {
+        get
+        {
+            return this._livesValue;
+        }
+
+        set
+        {
+            this._livesValue = value;
+            if (this._livesValue <= 0)
+            {
+                this._endGame();
+            }
+            else {
+                this.LivesLabel.text = "Lives: " + this._livesValue;
+            }
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
 		this._GenerateTanks ();
-	}
+        this.ScoreValue = 0;
+        this.LivesValue = 5;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,4 +67,19 @@ public class GameController : MonoBehaviour {
 			Instantiate(tank);
 		}
 	}
+    private void _endGame()
+    {
+        //this.HighScoreLabel.text = "High Score: " + this._scoreValue;
+        //this.GameOverLabel.enabled = true;
+        this.LivesLabel.enabled = false;
+        this.ScoreLabel.enabled = false;
+        //this.plane.gameObject.SetActive(false);
+        //this.island.gameObject.SetActive(false);
+        //this.HighScoreLabel.enabled = true;
+        //this._gameOverSound.Play();
+        //this.RestartButton.enabled = true;
+
+
+    }
+
 }
